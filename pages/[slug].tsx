33291eb8db50d951415e5bbcb26f8ad92e33309e
TKTBKDN <takhanhthient3@gmail.com>
2023-10-08 08:16:35 +0700

    Update [slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -31,11 +31,7 @@ export default function App({ url, title, description, icon, image }: any) {
 
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta name="og:title" content={title} />
-        <meta name="og:description" content={description} />
-        <meta name="og:image" content={image} />
+      <Head>       
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={icon} />
       </Head>
